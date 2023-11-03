6a7d4821eaef4f10fbe9333dff100f6e342f6361
Joshua Duenow <joshjduenow@gmail.com>
2023-11-03 16:35:53 -0500

    added onclick of calc buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,105 @@
 console.log('client.js is sourced!');
+
+function onReady() {
+    console.log("JavaScript is loaded!")
+  }
+  
+  onReady()
+  
+  let roundCounter =0;
+  
+  function onReset() {
+    // technichally don't need to clear guesses, but it 
+    // looked funny with them being there if playing multiple games
+    let rounds = document.getElementById('playerRound');
+    rounds.innerHTML = '';
+    roundCounter = 0;
+    axios({
+      method: 'POST',
+      url: '/randomNumber'
+    })
+  }
+  
+  function formSubmit(event) {
+    event.preventDefault()
+  }
+  
+  function onSubmit() {
+    
+    let playerOne = document.getElementById('playerOne').value;
+    let playerTwo = document.getElementById('playerTwo').value;
+    let playerThree = document.getElementById('playerThree').value;
+    let numberOne = document.getElementById('oneNumber').value;
+    let numberTwo = document.getElementById('twoNumber').value;
+    let numberThree = document.getElementById('threeNumber').value;
+    document.getElementById('playerOne').value = '';
+    document.getElementById('playerTwo').value = '';
+    document.getElementById('playerThree').value = '';
+    document.getElementById('oneNumber').value = '';
+    document.getElementById('twoNumber').value = '';
+    document.getElementById('threeNumber').value = '';
+    let newRound = [
+      {
+        playerName: playerOne,
+        playerNumber: Number(numberOne)
+      },
+      {
+        playerName: playerTwo,
+        playerNumber: Number(numberTwo),
+      },
+      {
+        playerName: playerThree,
+        playerNumber: Number(numberThree)
+      }]
+      roundCounter ++;
+      console.log(roundCounter);
+      console.log('expect array of input guesses', newRound);
+    axios({
+      method: 'POST',
+      url: '/round',
+      data: newRound
+    }).then((response) => {
+      getRound()
+    })
+    
+  }
+  
+  function getRound() {
+      axios({
+        url: '/round',
+        method: 'GET'
+    }).then((response) => {
+      let currentRound = response.data;
+      console.log('expect array of guess with results', currentRound);
+      renderRound(currentRound)
+    })
+  }
+  
+  function renderRound(currentRound){
+    let rounds = document.getElementById('playerRound');
+    rounds.innerHTML = '';
+    for (let i=0; i < currentRound.length; i++){
+      console.log('expect one person:', currentRound[i]);
+      rounds.innerHTML += `
+        <li>${currentRound[i].playerName} guessed ${currentRound[i].playerNumber} which is ${currentRound[i].numberComparison}</li>
+      `
+    }
+    document.getElementById('total-rounds').textContent = roundCounter;
+    
+    console.log('Expect a round counter');
+  }
+  //innerHtml
+    //will parse`` items as html within .js
+  
+  //textContent
+    //used to modify content in HTML (text)
+  
+  //value
+    //this is how we obtain text that a user types into
+    //types into and <input>
+    //if you set the value="" if the <input>
+    //it will store that value in your input box
+  
+  //querySelectory is used when an ID is not used
+  
+  //Radio Buttons research...
