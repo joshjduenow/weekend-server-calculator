cdb3dd11ec9783f2ec0f5c4a68df0225a66b3521
Joshua Duenow <joshjduenow@gmail.com>
2023-11-04 12:23:43 -0500

    fixed buttons and looking at server side now

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,27 +11,24 @@ function formSubmit(event) {
     event.preventDefault()
 }
 function onAdd() {
-    operator = "+";
     console.log("checking add function", operator);
+    return operator = "+";
+    
 }
 
 function onSubtract() {
-    operator = "-";
-
     console.log("checking subtract function",);
-
+    return operator = "-";
 }
 
 function onMultiply() {
-    operator = "*";
     console.log("checking multiply function",);
-
+    return operator = "*";
 }
 
 function onDivide() {
-    operator = "/";
     console.log("checking divide function",);
-
+    return operator = "/";
 }
 
 
@@ -40,20 +37,21 @@ function onEquals() {
     let secondNumber = document.getElementById('numberTwoIn').value;
     let newCalc = {
         firstNumber,
-        secondNumber,
-        operator
+        operator,
+        secondNumber
+        
     }
     document.getElementById('numberOneIn').value = '';
     document.getElementById('numberTwoIn').value = '';
     console.log("checking equal function", firstNumber, secondNumber, operator, newCalc);
-    //     axios({
-    //         url: '/result',
-    //         method: 'POST'
-    //     }).then((response) => {
-    //         console.log('response.data:', response.data)
-    //         let equations = response.data
-    //         renderEquation(equations)
-    //     })
+        axios({
+            url: '/calculations',
+            method: 'POST'
+        }).then((response) => {
+            console.log('response.data:', response.data)
+            let equations = response.data
+            //renderEquation(equations)
+        })
 }
 
 
